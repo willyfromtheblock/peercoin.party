a1fe2447c5ac15265b7c3ab5bb9eede677abcdab
bananenwilly <pussinbootsdev@gmail.com>
2019-09-27 21:23:25 +0200

    add sentry

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,11 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: "https://073d02f1e1e643018c96f7ea08ad9f82@sentry.io/1764111"
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
