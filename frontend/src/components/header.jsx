c4e91396cd4bb29b816f11ad58d2a11ce08920fe
bananenwilly <pussinbootsdev@gmail.com>
2019-09-29 21:36:22 +0200

    make header clickable to home

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -10,12 +10,14 @@ const Header = () => {
               className="col-md-2 col text-center"
               style={{ paddingRight: 0 }}
             >
-              <img
-                className="logo"
-                style={{ maxWidth: "100%", margin: 10, height: "3rem" }}
-                src="https://peercoinexplorer.net/peercoin-horizontal-greenleaf-whitetext-transparent.svg"
-                alt="Peercoin Logo"
-              />
+              <a href="/">
+                <img
+                  className="logo"
+                  style={{ maxWidth: "100%", margin: 10, height: "3rem" }}
+                  src="https://peercoinexplorer.net/peercoin-horizontal-greenleaf-whitetext-transparent.svg"
+                  alt="Peercoin Logo"
+                />
+              </a>
             </div>
             <div className="col-md-2 col-5 my-auto">
               <span className="my-auto header-text">...is art</span>
